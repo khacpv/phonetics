b9c72a5d49203c09847b7ef46f57b8dea43fae9e
khacpv <khacpv@gmail.com>
2021-03-07 23:16:20 +0700

    clean code

--- out/_next/static/I_OH7eV0FejDsZQwGOXcS/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-1a6318747c1ef8891a42.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-0733b917fc4e1fb68c15.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
