8c3cdc174c5c08413ec4cb4dcc0840eac2a31aa8
khacpv <khacpv@gmail.com>
2021-03-07 17:04:44 +0700

    update code

---
+++ components/Phonetic.jsx
@@ -0,0 +1,93 @@
+import React, { useCallback, useRef, useEffect } from 'react';
+
+const Phonetic = ({ ipa, sound, children, ...rest }) => {
+  const audioRef = useRef(null);
+  const playSound = useCallback(() => {
+    audioRef.current.play();
+  }, [sound]);
+  return (
+    <div className="phonetic">
+      <span>{ipa}</span>
+      <div onClick={playSound} className="sound">
+        <img src="/img/sound.png"></img>
+      </div>
+      <div className="popup">
+        <div className="popup-content">{children}</div>
+
+        <audio ref={audioRef} src={sound} className="audio">
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+
+      <style jsx>{`
+        .phonetic {
+          border: 1px solid lightgray;
+          width: 40px;
+          height: 40px;
+          text-align: center;
+          vertical-align: middle;
+          line-height: 40px;
+          white-space: nowrap;
+          font-size: 16px;
+          position: relative;
+          margin-right: 8px;
+        }
+        .phonetic:hover {
+          background: #dbdbdb;
+        }
+        .sound {
+          position: absolute;
+          right: 0;
+          top: 0;
+          opacity: 0;
+          padding: 8px;
+          width: 100%;
+          height: 100%;
+          z-index: 1;
+          cursor: pointer;
+          background: #797979;
+        }
+        .sound img {
+          width: 100%;
+          height: 100%;
+          object-fit: contain;
+        }
+        .popup {
+          position: absolute;
+          bottom: 0px;
+          left: -200px;
+          display: none;
+        }
+        .popup-content {
+          margin-bottom: 44px;
+          background: white;
+          flex-direction: column;
+          padding: 8px;
+          border: 0.5px solid #797979;
+          box-shadow: 3px 3px 8px #efefef;
+          overflow-y: auto;
+          width: auto;
+          height: auto;
+          min-width: 400px;
+          min-height: 100px;
+          pointer-events: all;
+          text-align: left;
+          vertical-align: top;
+          font-size: 14px;
+          line-height: 16px;
+        }
+        .audio {
+          display: none;
+        }
+        .phonetic:hover .popup {
+          display: flex;
+        }
+        .phonetic:hover .sound {
+          opacity: 1;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Phonetic;
--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,228 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Phonetic from '../components/Phonetic';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Phonetics</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a href="https://tophonetics.com/">Phonetics</a>
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Inspired by <a href="http://moonesl.vn/">MoonESL class</a>
         </p>
-
+        <h3>1. Nguyên âm (vowel)</h3>
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          {/* /ʌ/ */}
+          <Phonetic ipa="/ʌ/" sound="/audio/a-bus.mp3">
+            <span className="strong">Schwa được nhấn: /ʌ/</span>
+            Giống như âm /uh/ của tiếng việt
+            <br />
+            <span>1. Bus, But, Bug</span>
+            <br />
+            <span>2. My brother school has bus</span>
+          </Phonetic>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          {/* /ə/ */}
+          <Phonetic ipa="/ə/" sound="/audio/a-about.mp3">
+            <span className="strong">Schwa Không được nhấn: /ə/</span>
+            Chỉ giống như âm /ờ/ của tiếng việt nhưng nhẹ hơn
+            <br />
+            <span>1. About</span>
+            <br />
+            <span>2. Pronunciation is important</span>
+          </Phonetic>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          {/* /i/ */}
+          <Phonetic ipa="/i/" sound="/audio/i-fill.mp3">
+            <span className="strong">i lỏng miệng: /i/</span>
+            Chỉ hơi nhếch mép 2 bên 1 chút, nhẹ hơn âm /i/ của tiếng việt
+            <br />
+            <span>1. Hit, Bit</span>
+            <br />
+            <span>2. a seed on seat</span>
+          </Phonetic>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {/* /i:/ */}
+          <Phonetic ipa="/i:/" sound="/audio/i-feel.mp3">
+            <span className="strong">i căng miệng: /i:/ hoặc /ɪ/</span>
+            Miệng kéo sang 2 bên nhiều hơn so với âm /i/ lỏng miệng
+            <br />
+            <span>1. Seat, Seed, Heat, Week</span>
+            <br />
+            <span>2. A seed on seat</span>
+          </Phonetic>
+
+          {/* /e/ */}
+          <Phonetic ipa="/e/" sound="/audio/e-set.mp3">
+            <span className="strong">E lỏng miệng: /ɜ/ hoặc /e/</span>
+            Kéo miệng về 2 bên và hạ hàm 1 chút
+            <br />
+            <span>1. Set, Met, Get, Left</span>
+            <br />
+            <span>2. A set of pencil on the left of table</span>
+          </Phonetic>
+
+          {/* /æ/ */}
+          <Phonetic ipa="/æ/" sound="/audio/ae-map.mp3">
+            <span className="strong">A Căng miệng: /æ/</span>
+            Mở miệng đều về 4 phía
+            <br />
+            <span>1. Can, map</span>
+            <br />
+            <span>2. A map in the can</span>
+          </Phonetic>
+
+          {/* /ɒ/ */}
+          <Phonetic ipa="/ɒ/" sound="/audio/o-god.mp3">
+            <span className="strong">O hạ hàm: /ɒ/</span>
+            Chỉ kéo hàm xuống dưới thôi.
+            <br />
+            <span>1. God, Nod, Hot, Shop</span>
+            <br />
+            <span>2. A God in a shop will very hot</span>
+          </Phonetic>
+
+          {/* /ɑ:/ */}
+          {/* <Phonetic ipa="/ɑ:/" sound="/audio/a-hard.mp3">
+            <span className="strong">A ???: /ɑ:/</span>
+            <span>1. Hard</span>
+            <br />
+            <span>2. </span>
+          </Phonetic> */}
+
+          {/* /ɔ:/ */}
+          <Phonetic ipa="/ɔ:/" sound="/audio/o-fork.mp3">
+            <span className="strong">O tròn miệng: /ɔ:/</span>
+            Âm O căng & chu mỏ ra
+            <br />
+            <span>1. Fork, Ball, All, Call, Mall, Hall </span>
+            <br />
+            <span>2. A ball in the mall on the hall</span>
+          </Phonetic>
+
+          {/* /ɜ:/ */}
+          {/* <Phonetic ipa="/ɜ:/" sound="/audio/o-heard.mp3">
+            <span className="strong">Ơ căng miệng: /ɜ:/ (hoặc /e:/)</span>
+            <span>1. Heard</span>
+            <br />
+            <span>2. </span>
+          </Phonetic> */}
+
+          {/* /ʊ/ */}
+          <Phonetic ipa="/ʊ/" sound="/audio/u-put.mp3">
+            <span className="strong">U nhẹ: /ʊ/</span>
+            Giống chữ U của tiếng việt nhưng chỉ hơi cong môi 1 chút
+            <br />
+            <span>1. Put, Good, Pull</span>
+            <br />
+            <span>2. I put a budding in the hut</span>
+          </Phonetic>
+
+          {/* /u:/ */}
+          <Phonetic ipa="/u:/" sound="/audio/u-boot.mp3">
+            <span className="strong">U căng miệng: /u:/</span>
+            <span>1. Boot, Moon, Sure</span>
+            <br />
+            <span>2. I have a boot</span>
+          </Phonetic>
+        </div>
+
+        <h3>2. Nguyên âm Anh Mỹ (Diphthong vowels)</h3>
+        <div className="grid">
+          {/* /eɪ/ */}
+          <Phonetic ipa="/eɪ/" sound="/audio/ei-they.mp3">
+            <span className="strong">Ê nhẹ: /eɪ/</span>
+            Gần giống âm Ê của tiếng việt, nhưng ngắn hơn. Nếu theo sau là âm
+            /l/ thì phát âm giống với ÊU. vd: mail /meil/
+            <br />
+            <span>1. They, Made, Date, Day</span>
+            <br />
+            <span>2. They make a bake</span>
+          </Phonetic>
+
+          {/* /oʊ/ */}
+          <Phonetic ipa="/oʊ/" sound="/audio/ou-goal.mp3">
+            <span className="strong">ÂU : /oʊ/</span>
+            Nghe giống như âm /ÂU/ của tiếng việt
+            <br />
+            <span>1. Row, Goal, Soul, Cold, Goat, Note, Known, Know</span>
+            <br />
+            <span>2. How old are you?</span>
+            <br />
+            <span>3. I see a goat on the road</span>
+          </Phonetic>
+
+          {/* /aʊ/ */}
+          <Phonetic ipa="/aʊ/" sound="/audio/au-sound.mp3">
+            <span className="strong">AU: /aʊ/</span>
+            Nghe như âm A+U của tiếng việt. Chú ý khi kết hợp với âm /l/.
+            <br />
+            <span>1. Sound, How, Count, Cows</span>
+            <br />
+            <span>2. How to count the cows?</span>
+          </Phonetic>
+
+          {/* /ɪə/ */}
+          {/* <Phonetic ipa="/ɪə/" sound="/audio/ia-clear.mp3">
+            <span className="strong">IA: /ɪə/</span>
+            <br />
+            <span>1. Clear</span>
+            <br />
+            <span>2. My brother school has bus</span>
+          </Phonetic> */}
+
+          {/* /eə/ */}
+          {/* <Phonetic ipa="/eə/" sound="/audio/ea-care.mp3">
+            <span className="strong">AE: /eə/</span>
+            <br />
+            <span>1. Care</span>
+            <br />
+            <span>2. My brother school has bus</span>
+          </Phonetic> */}
+
+          {/* /ɔɪ/ */}
+          <Phonetic ipa="/ɔɪ/" sound="/audio/oi-coin.mp3">
+            <span className="strong">OI: /ɔɪ/</span>
+            Giống âm OI của tiếng việt
+            <br />
+            <span>1. Coin, Boy, Boil, Join, Annoy</span>
+            <br />
+            <span>2. The boy boils a coin</span>
+          </Phonetic>
+
+          {/* /aɪ/ */}
+          <Phonetic ipa="/aɪ/" sound="/audio/ai-find.mp3">
+            <span className="strong">AI: /aɪ/</span>
+            <span>1. Find, Ice, I'm, Eye</span>
+            <br />
+            <span>2. Find my iphone</span>
+          </Phonetic>
+
+          {/* /ʊə/ */}
+          {/* <Phonetic ipa="/ʊə/" sound="/audio/ua-tour.mp3">
+            <span className="strong">UƠ: /ʊə/</span>
+            <span>1. Tour</span>
+            <br />
+            <span>2. I'm a tour guide </span>
+          </Phonetic> */}
         </div>
       </main>
 
       <footer>
+        Powered by &nbsp;
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://facebook.com/khacpv"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          Khắc Phạm
         </a>
       </footer>
 
@@ -107,6 +274,11 @@ export default function Home() {
           text-decoration: none;
         }
 
+        .description a {
+          color: #ff4e48;
+          text-decoration: none;
+        }
+
         .title a:hover,
         .title a:focus,
         .title a:active {
@@ -137,7 +309,9 @@ export default function Home() {
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
-
+        h3 {
+          margin-top: 50px;
+        }
         .grid {
           display: flex;
           align-items: center;
@@ -145,7 +319,6 @@ export default function Home() {
           flex-wrap: wrap;
 
           max-width: 800px;
-          margin-top: 3rem;
         }
 
         .card {
@@ -182,6 +355,13 @@ export default function Home() {
           height: 1em;
         }
 
+        .strong {
+          font-size: 16px;
+          line-height: 16px;
+          font-weight: bold;
+          display: block;
+        }
+
         @media (max-width: 600px) {
           .grid {
             width: 100%;
@@ -205,5 +385,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
