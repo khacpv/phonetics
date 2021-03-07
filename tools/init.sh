29174311f2623ae8208c9c437ac46822bc959004
khacpv <khacpv@gmail.com>
2021-03-07 17:38:00 +0700

    update config

--- tools/init.sh
+++ tools/init.sh
@@ -1,1 +1,3 @@
 cp tools/oicphonetics.conf /etc/nginx/conf.d/
+systemctl reload nginx
+sudo certbot certonly --agree-tos --email khacpv@gmail.com --webroot -w /var/lib/letsencrypt/ -d phonetic.oicsoft.com
