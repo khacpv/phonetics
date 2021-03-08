6b6ae006f4801c8375d5f892502a2e35e9e31677
khacpv <khacpv@gmail.com>
2021-03-08 14:58:00 +0700

    release

--- out/_next/static/PKjnwVs5yzl-TknC1x__m/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-31a60fcc84c164e2c901.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-0733b917fc4e1fb68c15.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
